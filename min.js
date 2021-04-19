d5cb0923309f33750a3ec4454ce987c2bcf72a13
tk500 <62159155+tk500@users.noreply.github.com>
2021-04-19 15:51:02 +0900

    Update min.js

--- min.js
+++ min.js
@@ -6,43 +6,73 @@ script.setAttribute('type', 'text/javascript')
 script.addEventListener('load', function () {
 
   // ここにjQueryの記述をする
-  $(function() {
-    $('.slideshow').each(function() {
-      // スライド（画像）の数を取得
-      var $slides = $(this).find('img'),
-          slideNum = $slides.length,
-          currentIdx = 0; // 何番目か
-   
-      // 最初の画像をフェードイン
-      $(".slideshow img").eq(currentIdx).fadeIn();
-   
-      // 3秒後に次のスライドを表示
-      setTimeout(dispNextSlide, 3000);
-   
-      // 次のスライドを表示するメソッド
-      function dispNextSlide() {
-        var nextIdx = currentIdx + 1;
-   
-        // 最後のスライドの場合ははじめに戻る
-        // if (nextIdx > (slideNum - 1)) {
-        //   nextIdx = 0
-        // }
-        
-        // 現在のスライドをフェードアウト
-        $(".slideshow img").eq(currentIdx).fadeOut();
-        
-        // 次のスライドをフェードイン
-        $(".slideshow img").eq(nextIdx).fadeIn();
-        
-        // インデックスを更新
-        currentIdx = nextIdx;
-        
-      }
 
-    })
 
-  })
-  
+  // スライドショー
+  $(function(){
+
+
+    //（１）ページの概念・初期ページを設定
+    var page=0;
+    
+    //（２）イメージの数を最後のページ数として変数化
+    var lastPage =parseInt($("#slide img").length-1);
+    
+    //（３）最初に全部のイメージを一旦非表示
+    $("#slide img").css("display","none");
+    
+    //（４）初期ページを表示
+  $("#slide img" ).eq(page).css("display","block");
+    
+    //（５）ページ切換用、自作関数作成
+    function changePage(){
+      // １秒かけてフェードアウト
+      $("#slide img").fadeOut(1000);
+      $("#slide img").eq(page).fadeIn(1000);
+    };
+    
+    //（６）～秒間隔でイメージ切換の発火設定
+    var Timer;
+    function startTimer(){
+    Timer =setInterval(function(){
+      if(page === lastPage){
+        page = 0;
+        changePage();
+      }else{
+        page ++;
+        changePage();
+      };
+      },3000);
+    }
+    //（７）～秒間隔でイメージ切換の停止設定
+    function stopTimer(){
+    clearInterval(Timer);
+    }
+    
+    //（８）タイマースタート
+    startTimer();
+    
+    /*オプションを足す場合はここへ記載*/
+    
+    });
+
+    // フッタートグル
+  $(function(){
+  //要素をクリックしたら発動
+      $(".footer_navbar_t").on("click", function() {
+        // 擬似要素CSS変更
+          $(this).toggleClass('footer_navbar_t_b');
+      });
+    });
+
+  $(function(){
+    //id属性がtop_buttonの要素をクリックすると
+    $('#top_button').click(function(){
+     
+        //画面の上から0pxの所まで500ミリ秒（0.5秒）かけてアニメーションしながらスクロールする
+        $('html, body').animate({scrollTop: 0}, 500);
+    });
+  });
 
 })
 
